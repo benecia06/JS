a0d04c32fd496512955f388c8b2960c1895dd09e
benecia06 <goel.sakshi25@icloud.com>
2022-02-27 16:46:09 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+// STARTING JS-> JAVASCRIPT
+// VAR LET CONSTANT
+
+//VARIABLE DECLARATION
+var a; //int string float
+
+// VARIABLE INITIALISATION
+a=10;
+console.log(typeof a); //typeof: TELLS YOU THE TYPE OF VARIABLE
+console.log(a); //TELLS YOU THE VARIABLE VALUE
+a= "hello"; //re-initialising variable
+console.log(typeof a);
+console.log(a);
+
+a= true;
+console.log(typeof a);
+console.log(a);
